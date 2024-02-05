815c5438170e68cd455bde7ed4329bfbd9b414a1
Rahul Singh <rahulramola03@gmail.com>
2024-02-05 12:52:37 +0530

    1-10 videos done

---
+++ Concept.js
@@ -0,0 +1,15 @@
+console.log("hello")
+
+// const ,let, var
+
+// datatypes 
+// int,char,string,float,boolean,null,undefined,symbol
+
+// typeof(null) => object
+// typeof(undefined) => undefined"
+
+// Conversion to number = "33" => 33 , "33aa" => NaN ,true => 1 , false => 0 , null => 0 , undefined => NaN
+
+// All Premitive Values Goes into Stack and All Non-Primitive Values goes into Heap
+
+// in Stack its make a copy of data when you want previous data but in Heap it doesnt make copy it you change any data then all will change 
